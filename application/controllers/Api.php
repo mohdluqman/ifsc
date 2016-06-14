8c52c0308c66f94088ae95fcae62aca195ba8aca
mohdluqman <me@mohdluqman.com>
2016-06-14 17:33:11 +0530

    Added the parseXML function

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -2,6 +2,7 @@
 set_time_limit(0);
 defined('BASEPATH') OR exit('No direct script access allowed');
 include_once APPPATH."libraries/Simple_html_dom.php";
+include_once APPPATH."libraries/PHPExcel.php";
 
 class Api extends CI_Controller {
 
@@ -22,6 +23,63 @@ class Api extends CI_Controller {
 			//$this->db->insert('records', $data);
 		}
 
+	}
+
+	public function parse()
+	{
+		$que = $this->db->get_where("records",array("id >=" => "52"));
+		$res = $que->result_array();
+		foreach ($res as $result) {			
+			$url = str_replace("http:", "https:", $result["url"]);
+			$tempfile = sys_get_temp_dir()."/".$result["id"];
+			$data = file_put_contents($tempfile,file_get_contents($url));
+			$data = $this->parseXMLString($tempfile);
+			//$val = array();
+			for($i=2;$i<count($data);$i++) {
+				//$val[$i-2] = array(
+				$temp = array(
+					"ifsc" => isset($data[$i]["B"]) ? $data[$i]["B"] : "",
+					"micr" => isset($data[$i]["B"]) ? strval($data[$i]["B"]) : "",
+					"branch" => isset($data[$i]["B"]) ? $data[$i]["B"] : "",
+					"address" => isset($data[$i]["B"]) ? $data[$i]["B"] : "",
+					"contact" => isset($data[$i]["B"]) ? strval($data[$i]["B"]) : "",
+					"city" => isset($data[$i]["B"]) ? $data[$i]["B"] : "",
+					"district" => isset($data[$i]["B"]) ? $data[$i]["B"] : "",
+					"state" => isset($data[$i]["B"]) ? $data[$i]["B"] : "",
+					"bank_id" => isset($data[$i]["B"]) ? $data[$i]["B"] : "",
+					);
+				$this->db->insert("bank_detail",$temp);
+			}
+			//var_dump($val);
+			
+		}
+	}
+
+	public function parseXMLString($fileName='')
+	{
+		$excelReader = PHPExcel_IOFactory::createReaderForFile($fileName);
+		//if we dont need any formatting on the data
+		$excelReader->setReadDataOnly();
+		 
+		//load only certain sheets from the file
+		//$loadSheets = array('Sheet1', 'Sheet2');
+		//$excelReader->setLoadSheetsOnly($loadSheets);
+		 
+		//the default behavior is to load all sheets
+		$excelReader->setLoadAllSheets();
+		$objPHPExcel = $excelReader->load($fileName);
+		$data = $objPHPExcel->getActiveSheet()->toArray(null, true,true,true);
+		//var_dump($data[1]);
+		return $data;
+	}
+
+	public function AddBankFromDb()
+	{
+		$query = $this->db->get('records');
+		foreach ($query->result_array() as $value) {
+			$str = file_get_contents($value["url"]);
+
+		}
 	}
 
 	public function checkModified()
@@ -60,7 +118,6 @@ class Api extends CI_Controller {
 		);
 
 		$h = get_headers($url, 1);
-		var_dump($h);
 		if (!empty($h) && strpos($h[0],"304") !== FALSE)
 			return false;
 		else
@@ -71,7 +128,6 @@ class Api extends CI_Controller {
 	{
 		$url = str_replace("http:", "https:", $url);
 		$h = get_headers($url, 1);
-		//var_dump($h);
 		if (!empty($h))
 			return $h['Last-Modified'];
 	}
