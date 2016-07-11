c82d0d70d0e60c774032345645a8ed40e2d9b72b
mohdluqman <luqman.geeky@gmail.com>
2016-07-11 04:57:19 -0400

    Danish V1 design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 ifsc_app = angular.module('ifsc-bank',['ngRoute','angularCSS','ngclipboard']);
 
 ifsc_app.controller('homeCtrl',function($scope,$http){
+
+
 	$http.get('/s/get_all_banks').then(function(data){
 		$scope.banksname = data
 	})
@@ -10,15 +12,33 @@ ifsc_app.controller('homeCtrl',function($scope,$http){
 	$http.get('/s/get_all_districts').then(function(data){
 		$scope.districts = data
 	})*/
-	var parm = JSON.stringify({bank_id:4});
-	console.log(parm)
-	$http.post('http://bankslibrary.com/s/get_all_banks',parm).then(function(data){
-		$scope.districts = data
+	$scope.send_bank_name_id = function(){
+			var bankid = $scope.bank_name.id;
+			console.log(bankid)
+		// $http.post("http://bankslibrary.com/s/get_state_from_bank",{bank_id:bankid},headers: {'Content-Type':'application/x-www-form-urlencoded'}).then(function(data){
+		// $scope.states = data
+		// console.log(data)
+		// })	
+
+
+		$http({
+		    method: 'POST',
+		    url: 'http://bankslibrary.com/s/get_state_from_bank',
+		    headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+		    transformRequest: function(obj) {
+		        var str = [];
+		        for(var p in obj)
+		        str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+		        return str.join("&");
+		    },
+		    data: {bank_id:bankid}
+		}).then(function(data){
+		$scope.states = data
 		console.log(data)
-	})
-	// $http.get('/s/get_all_branchs').then(function(data){
-	// 	$scope.branchs = data
-	// })
+		})	
+	}
+
+
 })
 
 ifsc_app.config(['$routeProvider',function($routeProvider){
